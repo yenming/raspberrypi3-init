bc58f53ec22ba56c0618bff0b0ef10c7df7721db
yenming <yenming0212@gmail.com>
2018-10-16 16:42:50 +0800

    add network-setup

---
+++ pi3-network-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Program:
+# This is Pi3-network-setup 
+# History:
+# 2018/10/16
+
+PATH=/etc/wpa_supplicant/
+export PATH
+
+read -p "Please input your network name(ssid):" name      # 提示網路名稱（SSID）
+read -p "Please input your network password:" password       # 提示使用者密碼
+echo "\nYour network name(ssid): ${name}" # 結果由螢幕輸出
+echo "\nYour password is: ${password}\n" # 結果由螢幕輸出
+read -p "Please check your network Name(ssid) and Password are true or false :" check
+
+if [ "${check}" == "true"]; then
+	echo "OK, Good"
+	exit 0
+fi
+
+echo "I don't know what your choice is" && exit 0
+
+# The Program End
+exit 0 
