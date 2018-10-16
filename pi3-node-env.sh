0768003ec8f7b47d1cefb54ceab84f7b351032c8
yenming <yenming0212@gmail.com>
2018-10-16 16:36:59 +0800

    Pi3-node-env add \n

--- pi3-node-env.sh
+++ pi3-node-env.sh
@@ -5,22 +5,22 @@
 # History:
 # 2018/10/16
 
-read -p "##### Update your system package list #####" ;
+read -p "\n##### Update your system package list #####\n" ;
 sudo apt-get update;
 
-read -p "##### Upgrade all your installed packages to their latest version #####" ;
+read -p "\n##### Upgrade all your installed packages to their latest version #####\n" ;
 sudo apt-get dist-upgrade;
 
-read -p "##### To download and install newest version of Node.js, use the following command #####" 
+read -p "\n##### To download and install newest version of Node.js, use the following command #####\n" 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -;
 
-read -p "##### Now install it by running #####" ;
+read -p "\n##### Now install it by running #####\n" ;
 sudo apt-get install -y nodejs;
 
-read -p "##### Check that the installation was successful, and the version number of Node.js #####" ;
+read -p "\n##### Check that the installation was successful, and the version number of Node.js #####\n" ;
 node -v;
 
-read -p "##### Now install Finish #####" ;
+read -p "\n##### Now install Finish #####\n" ;
 
 # The Program End
 exit 0 
